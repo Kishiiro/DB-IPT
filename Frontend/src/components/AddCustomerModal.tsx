705928221b0c107578c91db2599d3ba5c156b09b
Joshua Gida <71809017+realdiceshorty@users.noreply.github.com>
2023-05-21 23:44:12 +0800

    Customer Edits

---
+++ Frontend/src/components/AddCustomerModal.tsx
@@ -0,0 +1,248 @@
+import { useRef } from "react";
+import Modal from "./modal";
+import Axios from "axios";
+
+const AddCustomerModal = ({ setShowModal, setReload }: any) => {
+  const customerNumber  = useRef<any>("");
+  const customerName = useRef<any>("");
+  const contactLastNameRef = useRef<any>("");
+  const contactFirstNameRef = useRef<any>("");
+  const phoneRef = useRef<any>("");
+  const addressLine1Ref = useRef<any>("");
+  const addressLine2Ref = useRef<any>(null);
+  const cityRef = useRef<any>("");
+  const stateRef = useRef<any>(null);
+  const postalCodeRef = useRef<any>(null);
+  const countryRef = useRef<any>("");
+  const salesRepEmployeeNumberRef  = useRef<any>(null);
+  const creditLimitRef   = useRef<any>(null);
+
+  const submitHandler = async (e: any) => {
+    e.preventDefault();
+    const data = {
+      customerNumber: customerNumber.current.value,
+      customerName: customerName.current.value,
+      contactLastNameRef: contactLastNameRef.current.value,
+      contactFirstNameRef: contactFirstNameRef.current.value,
+      phoneRef: phoneRef.current.value,
+      addressLine1Ref: addressLine1Ref.current.value,
+      addressLine2Ref: addressLine2Ref.current.value,
+      cityRef: cityRef.current.value,
+      stateRef: stateRef.current.value,
+      postalCodeRef: Number(postalCodeRef.current.value),
+      countryRef: countryRef.current.value,
+      salesRepEmployeeNumberRef: Number(salesRepEmployeeNumberRef.current.value),
+      creditLimitRef: Number(creditLimitRef.current.value),
+    };
+
+    const response = await Axios.post("/offices/create", data);
+    setReload((prev: any) => prev + 1);
+    setShowModal(false);
+    console.log(response.data);
+  };
+
+  return (
+    <Modal
+      onClick={() => setShowModal(false)}
+      className="flex flex-col justify-center items-center bg-white"
+    >
+      <h1 className="font-bold text-[2rem] mb-[1.5rem] text-blue-500">
+        Add Customer
+      </h1>
+      <form onSubmit={submitHandler}>
+        <div className="flex gap-8 items-center">
+          <div>
+            <div className="flex items-center gap-4">
+              <div className="mb-4">
+                <label
+                  htmlFor="customerNumber"
+                  className="font-semibold text-[1.15rem]"
+                >
+                  Customer Number
+                </label>
+                <input
+                  ref={customerNumber}
+                  id="customerNumber"
+                  type="text"
+                  className="block mt-[0.5rem] bg-white border border-solid border-gray-300 h-[2.5rem] w-[18rem] outline-none p-[1rem] rounded"
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  htmlFor="customerName"
+                  className="font-semibold text-[1.15rem]"
+                >
+                  Customer Name
+                </label>
+                <input
+                  ref={customerName}
+                  id="customerName"
+                  type="text"
+                  className="block mt-[0.5rem] bg-white border border-solid border-gray-300 h-[2.5rem] w-[18rem] outline-none p-[1rem] rounded"
+                />
+              </div>
+              <div className="block mb-4">
+                <label
+                  htmlFor="contactLastName"
+                  className="font-semibold text-[1.15rem]"
+                >
+                  Contact Last Name
+                </label>
+                <input
+                  ref={contactLastNameRef}
+                  id="contactLastName"
+                  type="text"
+                  className="block mt-[0.5rem] bg-white border border-solid border-gray-300 h-[2.5rem] w-[18rem] outline-none p-[1rem] rounded"
+                />
+              </div>
+              <div className="mb-[1rem]">
+                <label
+                  htmlFor="contactFirstName"
+                  className="font-semibold text-[1.15rem]"
+                >
+                  Contact First Name
+                </label>
+                <input
+                  ref={contactFirstNameRef}
+                  id="contactFirstName"
+                  type="text"
+                  className="block mt-[0.5rem] bg-white border border-solid border-gray-300 h-[2.5rem] w-[18rem] outline-none p-[1rem] rounded"
+                />
+              </div>
+              <div className="flex items-center gap-4">
+                <div className="mb-[1rem]">
+                  <label
+                    htmlFor="phone"
+                    className="font-semibold text-[1.15rem]"
+                  >
+                    Phone
+                  </label>
+                  <input
+                    ref={phoneRef}
+                    id="phone"
+                    type="text"
+                    className="block mt-[0.5rem] bg-white border border-solid border-gray-300 h-[2.5rem] w-[18rem] outline-none p-[1rem] rounded"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="flex items-center gap-4">
+              <div className="flex items-center gap-4">
+                <div className="mb-[1rem]">
+                  <label
+                    htmlFor="addressLine1"
+                    className="font-semibold text-[1.15rem]"
+                  >
+                    Address Line 1
+                  </label>
+                  <input
+                    ref={addressLine1Ref}
+                    id="addressLine1"
+                    type="text"
+                    className="block mt-[0.5rem] bg-white border border-solid border-gray-300 h-[2.5rem] w-[18rem] outline-none p-[1rem] rounded"
+                  />
+                </div>
+              </div>
+              <div className="flex items-center gap-4">
+                <div className="mb-[1rem]">
+                  <label
+                    htmlFor="addressLine2"
+                    className="font-semibold text-[1.15rem]"
+                  >
+                    Address Line 2
+                  </label>
+                  <input
+                    ref={addressLine2Ref}
+                    id="addressLine2"
+                    type="text"
+                    className="block mt-[0.5rem] bg-white border border-solid border-gray-300 h-[2.5rem] w-[18rem] outline-none p-[1rem] rounded"
+                  />
+                </div>
+              </div>
+              <div className="block mb-4">
+                <label
+                  htmlFor="city"
+                  className="font-semibold text-[1.15rem]"
+                >
+                  City
+                </label>
+                <input
+                  ref={cityRef}
+                  id="city"
+                  type="text"
+                  className="block mt-[0.5rem] bg-white border border-solid border-gray-300 h-[2.5rem] w-[18rem] outline-none p-[1rem] rounded"
+                />
+              </div>
+              <div className="block mb-4">
+                <label htmlFor="state" className="font-semibold text-[1.15rem]">
+                  State
+                </label>
+                <input
+                  ref={stateRef}
+                  id="postalCode"
+                  type="text"
+                  className="block mt-[0.5rem] bg-white border border-solid border-gray-300 h-[2.5rem] w-[18rem] outline-none p-[1rem] rounded"
+                />
+              </div>
+              <div className="block mb-4">
+                <label htmlFor="state" className="font-semibold text-[1.15rem]">
+                  Postal Code
+                </label>
+                <input
+                  ref={postalCodeRef}
+                  id="postalCode"
+                  type="text"
+                  className="block mt-[0.5rem] bg-white border border-solid border-gray-300 h-[2.5rem] w-[18rem] outline-none p-[1rem] rounded"
+                />
+              </div>
+              
+              <div className="block mb-4">
+                <label htmlFor="country" className="font-semibold text-[1.15rem]">
+                  State
+                </label>
+                <input
+                  ref={countryRef}
+                  id="country"
+                  type="text"
+                  className="block mt-[0.5rem] bg-white border border-solid border-gray-300 h-[2.5rem] w-[18rem] outline-none p-[1rem] rounded"
+                />
+              </div>
+              <div className="block mb-4">
+                <label htmlFor="salesRepEmployeeNumber" className="font-semibold text-[1.15rem]">
+                  State
+                </label>
+                <input
+                  ref={salesRepEmployeeNumberRef}
+                  id="salesRepEmployeeNumber"
+                  type="text"
+                  className="block mt-[0.5rem] bg-white border border-solid border-gray-300 h-[2.5rem] w-[18rem] outline-none p-[1rem] rounded"
+                />
+              </div>
+              <div className="block mb-4">
+                <label htmlFor="creditLimit" className="font-semibold text-[1.15rem]">
+                  State
+                </label>
+                <input
+                  ref={creditLimitRef}
+                  id="creditLimit"
+                  type="text"
+                  className="block mt-[0.5rem] bg-white border border-solid border-gray-300 h-[2.5rem] w-[18rem] outline-none p-[1rem] rounded"
+                />
+              </div>
+            </div>
+            <div className="text-center">
+              <button
+                type="submit"
+                className="bg-blue-500 mt-[1.5rem] text-white h-[2.8rem] rounded w-[20rem] font-semibold transition-all duration-[0.3s] ease-in-out hover:bg-blue-600"
+              >
+                Add Customer
+              </button>
+            </div>
+          </div>
+        </div>
+      </form>
+    </Modal>
+  );
+};
+
+export default AddCustomerModal;
--- Frontend/src/pages/Customer.tsx
+++ Frontend/src/pages/Customer.tsx
@@ -1,8 +1,165 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import Axios from "axios";
 
-function Customer() {
-  return <div>Customer</div>;
-}
+import AddCustomerModal from "../components/AddCustomerModal";
+import EditCustomerModal from "../components/EditCustomerModal";
+
+const Customer = () => {
+  const [customers, setCustomer] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [showEditModal, setShowEditModal] = useState(false);
+  const [editCustomer, setEditCustomer] = useState<any>({});
+  const [reload, setReload] = useState(0);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const ourRequest = Axios.CancelToken.source();
+    const fetchPost = async () => {
+      try {
+        const response = await Axios.get("/Customers", {
+          cancelToken: ourRequest.token,
+        });
+        setCustomer(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchPost();
+    return () => {
+      ourRequest.cancel();
+    };
+  }, [reload]);
+
+  return (
+    <>
+      {showModal && (
+        <AddCustomerModal setReload={setReload} setShowModal={setShowModal} />
+      )}
+      {showEditModal && (
+        <EditCustomerModal
+          editCustomer={editCustomer}
+          setReload={setReload}
+          setShowEditModal={setShowEditModal}
+        />
+      )}
+      <div className="bg-blue-500 w-full h-14 p-8 items-center flex">
+        <h1 className="text-white font-bold text-3xl">Customers</h1>
+      </div>
+      <div className="container md:mx-auto mt-8 mb-6">
+        <div className=" text-right mb-6">
+          <button
+            onClick={() => setShowModal(true)}
+            className="bg-green-500 hover:bg-green-600 duration-300 transition-all ease-in-out text-white font-semibold py-2 px-4 rounded"
+          >
+            + Add
+          </button>
+          <button
+          onClick={() => navigate("/")}
+          className="bg-red-500 hover:bg-neutral-500 duration-300 transition-all ease-in-out text-white font-semibold py-2 px-4 rounded"
+        >
+          Back
+        </button>
+        </div>
+        <table
+          cellPadding={10}
+          className=" text-center h-auto w-full border  border-black "
+        >
+          <thead className="h-[20px] min-h-[1em] w-px self-stretch bg-gradient-to-tr from-transparent via-neutral-500 to-transparent opacity-20 dark:opacity-100 ">
+            <tr>
+              <th className="py-2 px-4">Customer Number</th>
+              <th className="py-2 px-4">Customer Name </th>
+              <th className="py-2 px-4">Contact Last Name</th>
+              <th className="py-2 px-4">Contact First Name</th>
+              <th className="py-2 px-4">Phone</th>
+              <th className="py-2 px-4">Address Line 1</th>
+              <th className="py-2 px-4">Address Line 2</th>
+              <th className="py-2 px-4">City</th>
+              <th className="py-2 px-4">State</th>
+              <th className="py-2 px-4">Postal Code</th>
+              <th className="py-2 px-4">Country</th>
+              <th className="py-2 px-4">Employee Number</th>
+              <th className="py-2 px-4">Credit Limit</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody className="h-[20px] min-h-[1em] w-px self-stretch bg-gradient-to-tr from-transparent via-neutral-500 to-transparent opacity-20 dark:opacity-100">
+            {customers.map((customer: any, index: number) => (
+              <>
+              <tr key={customer.customerCode}>
+                <td>{customer.customerNumber}</td>
+                <td>{customer.customerName}</td>
+                <td>{customer.contactLastName}</td>
+                <td>{customer.contactFirstName}</td>
+                <td>{customer.phone}</td>
+                <td>{customer.addressLine1}</td>
+                <td>{customer.addressLine2}</td>
+                <td>{customer.city}</td>
+                <td>{customer.state}</td>
+                <td>{customer.postalCode}</td>
+                <td>{customer.country}</td>
+                <td>{customer.salesRepEmployeeNumber}</td> 
+                <td>{customer.creditLimit}</td> 
+                <td>
+                  <button
+                    onClick={async () => {
+                      setEditCustomer({
+                        customerNumber: customer.customerNumber,
+                        customerName: customer.customerName,
+                        contactLastName: customer.contactLastName,
+                        contactFirstName: customer.contactFirstName,
+                        phone: customer.phone,
+                        addressLine1: customer.addressLine1,
+                        addressLine2: customer.addressLine2,
+                        city: customer.city,
+                        state: customer.state,
+                        postalCode: customer.postalCode,
+                        country: customer.country,
+                        salesRepEmployeeNumber: customer.salesRepEmployeeNumber,
+                        creditLimit: customer.creditLimit,
+                      });
+                      setShowEditModal(true);
+                    }}
+                    className="m-6 bg-blue-400 hover:bg-blue-600 duration-300 transition-all ease-in-out text-white font-bold py-2 px-4 rounded"
+                  >
+                    &#9998;
+                  </button>
+                  <button
+                    onClick={async () => {
+                      try {
+                        var result = confirm("Want to delete?");
+                        if (result) {
+
+                          const response = await Axios.delete(
+                            `customers/${customer.customerNumber}`
+                          );
+                          console.log(response.data);
+                          setReload((prev) => prev + 1);
+                        }
+                      } catch (e) {
+                        console.log(e);
+                      }
+                    }}
+                    className="bg-red-400 hover:bg-red-600 duration-300 transition-all ease-in-out text-white font-bold py-2 px-4 rounded"
+                  >
+                    &times;
+                  </button>
+                </td>
+              </tr>
+              {index !== customers.length - 1 && (
+                  <tr className="spacing-row">
+                    <td colSpan={14} className="h-4">
+                      <hr className="border-gray-400" />
+                    </td>
+                  </tr>
+                )}
+              </>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+};
 
 export default Customer;
